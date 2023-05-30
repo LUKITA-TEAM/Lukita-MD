c0f97cc65278e56dbea4270a10dd31d5ef9cdca3
ptsuardana01 <ptsuardana01@gmail.com>
2023-05-31 02:20:45 +0800

    setup WelcomeScreen

--- app/src/main/java/com/lukitateam/lukita/ui/screen/welcome/WelcomeScreen1.kt
+++ app/src/main/java/com/lukitateam/lukita/ui/screen/welcome/WelcomeScreen1.kt
@@ -2,10 +2,20 @@ package com.lukitateam.lukita.ui.screen.welcome
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lukitateam.lukita.ui.theme.LukitaTheme
 
 @Composable
 fun WelcomeScreen1(
     modifier: Modifier = Modifier,
 ) {
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomeScreen1Preview() {
+    LukitaTheme {
+        WelcomeScreen1()
+    }
 }
