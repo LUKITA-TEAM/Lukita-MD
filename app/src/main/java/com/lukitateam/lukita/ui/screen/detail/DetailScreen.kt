687f2a7f4718c9d5eeff0acd5c509022efda2099
ptsuardana01 <ptsuardana01@gmail.com>
2023-06-03 18:06:35 +0800

    setup DetailScreen

---
+++ app/src/main/java/com/lukitateam/lukita/ui/screen/detail/DetailScreen.kt
@@ -0,0 +1,21 @@
+package com.lukitateam.lukita.ui.screen.detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lukitateam.lukita.ui.theme.LukitaTheme
+
+@Composable
+fun DetailScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetailScreenPreview() {
+    LukitaTheme {
+        DetailScreen()
+    }
+}
